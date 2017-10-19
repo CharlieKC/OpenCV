c3a5d60cec721206ced8b9ef22247382c02bf8df
Charlie Kruczko <ck17420@it052590.users.bris.ac.uk>
2017-10-19 09:53:50 +0100

    added a comment to test github

--- convolution.cpp
+++ convolution.cpp
@@ -3,12 +3,12 @@
 #include <opencv/cv.h>        //you may need to
 #include <opencv/highgui.h>   //adjust import locations
 #include <opencv/cxcore.h>    //depending on your machine setup
-
+//blah
 
 using namespace cv;
 
 void GaussianBlur(
-	cv::Mat &input, 
+	cv::Mat &input,
 	int size,
 	cv::Mat &blurredOutput);
 
@@ -44,10 +44,10 @@ void GaussianBlur(cv::Mat &input, int size, cv::Mat &blurredOutput)
 	// intialise the output using the input
 	blurredOutput.create(input.size(), input.type());
 
-	// create the Gaussian kernel in 1D 
+	// create the Gaussian kernel in 1D
 	cv::Mat kX = cv::getGaussianKernel(size, -1);
 	cv::Mat kY = cv::getGaussianKernel(size, -1);
-	
+
 	// make it 2D multiply one by the transpose of the other
 	cv::Mat kernel = kX * kY.t();
 
@@ -60,13 +60,13 @@ void GaussianBlur(cv::Mat &input, int size, cv::Mat &blurredOutput)
 	int kernelRadiusY = ( kernel.size[1] - 1 ) / 2;
 
 	cv::Mat paddedInput;
-	cv::copyMakeBorder( input, paddedInput, 
+	cv::copyMakeBorder( input, paddedInput,
 		kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
 		cv::BORDER_REPLICATE );
 
 	// now we can do the convoltion
 	for ( int i = 0; i < input.rows; i++ )
-	{	
+	{
 		for( int j = 0; j < input.cols; j++ )
 		{
 			double sum = 0.0;
@@ -85,7 +85,7 @@ void GaussianBlur(cv::Mat &input, int size, cv::Mat &blurredOutput)
 					double kernalval = kernel.at<double>( kernelx, kernely );
 
 					// do the multiplication
-					sum += imageval * kernalval;							
+					sum += imageval * kernalval;
 				}
 			}
 			// set the output value as the sum of the convolution
